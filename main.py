a279158667063dc5ae7d82eea9a590e2e19d48e0
antlis <antlis850@gmail.com>
2024-02-21 02:31:59 +0500

    Update main.py more comments

--- main.py
+++ main.py
@@ -23,21 +23,80 @@ def decode(message_file):
     width = 1  # number of items on the same line in the triangle
     last = 0   # index of the last item on the current line in the triangle
 
-    # https://www.w3schools.com/python/python_iterators.asp
-    # https://wiki.python.org/moin/Iterator
+    # https://wiki.python.org/moin/Generators
+    # https://www.w3schools.com/python/ref_func_map.asp
+    # https://www.w3schools.com/python/ref_string_split.asp
+    # !!! you need to turn key from string to integer with int(key)
+    # if you wont do that, sorted function below won't work correctly
+    # would consider 10 go before 9 for example
     words_generator = ((int(key), word) for key, word in map(str.split, words_arr))
 
+    # https://www.w3schools.com/python/ref_func_sorted.asp
     words_sorted = sorted(words_generator)
 
     # https://pythonbasics.org/enumerate/
     words_enumerator = enumerate(words_sorted)
 
-    result_arr = []
+    #################################
+    # in case you passing index-2.txt
+    # 1 i - index 0
+    # 2 something - index 1
+    # 4 else - index 3
+    # 3 love - index 2
+    # 5 dog - index 4
+    # 6 computers - index 5
 
-    for i, (_, word) in words_enumerator:
-        if i == last and (last := last + (width := width + 1)):
+    #################################
+    # i love computers
+    # 1 (i)
+    # 2 3 (love)
+    # 4 5 6 (computers)
+    #################################
+
+    # -------------------------------
+    # First iteration index == last | 0 == 0
+    # you append first word - `i`, with index 0, to result_arr
+    # then you assign new value to width, width = width + 1, width = 1 + 1, width = 2
+    # also you assign new value to last, last = last + width, last = 0 + 2, last = 2
+    # -------------------------------
+    # Second iteration index 1 not equal last which is 2
+    # -------------------------------
+    # Thired iteration index == last | 2 == 2
+    # you append word with idnex 2 to result array - love
+    # then you assign new value to width, width = width + 1, width = 2 + 1, width = 3
+    # also you assign new value to last, last = last + width, last = 2 + 3, last = 5
+    # -------------------------------
+    # Fourth iteration index 3 not equal last which is 5
+    # -------------------------------
+    # Fifth iteration index 4 not equal last which is 5
+    # -------------------------------
+    # Sixth iteration index == last | 5 == 5
+    # you append word with idnex 5 to result array - computers
+    # ......
+    #################################
+
+    # https://www.w3schools.com/python/python_iterators.asp
+    # https://wiki.python.org/moin/Iterator
+    # (_, word), underscore here is line key, which is not important, cause not used
+    # We rely only on index
+    result_arr = []
+    for index, (_, word) in words_enumerator:
+        if index == last:
+            width = width + 1
+            last = last + width
             result_arr.append(word)
-            # print('i: ', + i)
+            # print('i: ', + index)
+            # print('_: ', + _)
+            # print('word: ' + word)
+            # print('width: ' + str(width))
+            # print('last: ' + str(last))
+            # print('-------------------')
+
+        # Shorter version
+        #
+        # if index == last and (last := last + (width := width + 1)):
+        #     result_arr.append(word)
+            # print('i: ', + index)
             # print('_: ', + _)
             # print('word: ' + word)
             # print('width: ' + str(width))
@@ -46,11 +105,13 @@ def decode(message_file):
 
     return " ".join(result_arr)
 
+    # Shorter version
+    #
     # return  " ".join(
-    #     word for i, (_, word) in words_enumerator
+    #     word for index, (_, word) in words_enumerator
     #     # Explanation of := operator
     #     # https://stackoverflow.com/questions/10405820/what-is-the-operator
-    #     if i == last and (last := last + (width := width + 1))
+    #     if index == last and (last := last + (width := width + 1))
     # )
 
 print('\n')
